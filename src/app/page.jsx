e06c969af0faea18e4b3546ffdf09df8c8b62ab2
onissen <56229919+onissen@users.noreply.github.com>
2024-05-27 17:41:12 +0200

    Working global request

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,31 @@
-const HomePage = async () => {
+import { auth } from "@/lib/auth";
+import directus from "@/lib/directus";
+import { readItems, withToken } from "@directus/sdk";
 
+async function getData() {
+  const session = await auth()
+  if (session) {
+    return await directus.request(withToken(session.user.accessToken,readItems("global")))
+  }
+}
+
+const HomePage = async () => {
+  // const session = await auth()
+  // console.log(session)
+  const data = await getData()
+  if (data) {
+    return (
+      <div>
+       <h1>{data.title}</h1> 
+       <p>{data.description}</p>
+      </div>
+    );
+  }
   return (
     <div>
-     <h1>Home page</h1> 
+      <h1>Home Page</h1>
     </div>
-  );
+  )
 };
 
 export default HomePage;
